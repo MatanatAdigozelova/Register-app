87fd2dd43928c1e53313d097493710e0fffdd08f
MatanatAdigozelova <adigozelovamata@gmail.com>
2024-01-18 16:16:11 +0400

    update winforms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,7 +36,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.selecGroup = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.or = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,7 +55,7 @@
             // buttonAdd
             // 
             this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonAdd.Location = new System.Drawing.Point(169, 200);
+            this.buttonAdd.Location = new System.Drawing.Point(169, 189);
             this.buttonAdd.Name = "buttonAdd";
             this.buttonAdd.Size = new System.Drawing.Size(173, 44);
             this.buttonAdd.TabIndex = 1;
@@ -67,9 +68,9 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(-2, 297);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(566, 235);
+            this.dataGridView1.Size = new System.Drawing.Size(986, 235);
             this.dataGridView1.TabIndex = 2;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // textName
             // 
@@ -110,6 +111,7 @@
             // 
             // selecGroup
             // 
+            this.selecGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.selecGroup.FormattingEnabled = true;
             this.selecGroup.Location = new System.Drawing.Point(169, 162);
             this.selecGroup.Name = "selecGroup";
@@ -117,21 +119,34 @@
             this.selecGroup.TabIndex = 8;
             this.selecGroup.SelectedIndexChanged += new System.EventHandler(this.selecGroup_SelectedIndexChanged);
             // 
-            // label4
+            // buttonUpdate
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(446, 189);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "label4";
+            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonUpdate.Location = new System.Drawing.Point(169, 252);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(173, 44);
+            this.buttonUpdate.TabIndex = 9;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // or
+            // 
+            this.or.AutoSize = true;
+            this.or.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.or.Location = new System.Drawing.Point(239, 236);
+            this.or.Name = "or";
+            this.or.Size = new System.Drawing.Size(20, 13);
+            this.or.TabIndex = 10;
+            this.or.Text = "Or";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 526);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(513, 526);
+            this.Controls.Add(this.or);
+            this.Controls.Add(this.buttonUpdate);
             this.Controls.Add(this.selecGroup);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -159,7 +174,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox selecGroup;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Label or;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,12 +13,40 @@ namespace WindowsForm
 
     public partial class Form1 : Form
     {
-        List<Student> Groups = new List<Student>();
+
+        List<Student> studentlist = new List<Student>();
+        List<Groupss> groupslist = new List<Groupss>();
+        int indexrow;
         public Form1()
         {
 
+
+
+
+            Groupss group1 = new Groupss("P118");
+            Groupss group2 = new Groupss("P114");
+            Groupss group3 = new Groupss("P112");
+            groupslist.Add(group1);
+            groupslist.Add(group2);
+            groupslist.Add(group3);
             InitializeComponent();
+            //selecGroup.Items.Add(group1.GName);
+            //selecGroup.Items.Add(group2);
+            //selecGroup.Items.Add(group3);
+
+            foreach (var item in groupslist)
+            {
+                selecGroup.Items.Add(item.GName);
+
+            }
+            buttonUpdate.Visible= false;
+            or.Visible=false;
+
+
+
+
         }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -38,44 +66,105 @@ namespace WindowsForm
         private void buttonAdd_Click(object sender, EventArgs e)
 
         {
-            List<Groups> groupslist = new List<Groups>();
-            Groups groups = new Groups(selecGroup.Text);
+
+            //Groupss groups = new Groupss(selecGroup.Text);
+            string groupname = selecGroup.Text;
             Student student = new Student(textName.Text, textSurname.Text);
-            Groups.Add(student);
-            MessageBox.Show($"{student.Name} Added");
-            textName.Text = "";
-            textSurname.Text = "";
-            dataGridView1.DataSource = "";
-            dataGridView1.DataSource = Groups;
-            Groups groups1 = new Groups("p118");
-            Groups groups2 = new Groups("p119");
-            groupslist.Add(groups1);
-            groupslist.Add(groups2);
+            if (textName.Text == "" || textSurname.Text == "" || groupname == "")
+            {
+                MessageBox.Show("ERROR");
+            }
+            else
+            {
+                foreach (var item in groupslist)
+                {
+                    if (item.GName == groupname)
+                    {
+                        student.GroupsId = groupname;
+                    }
+                }
+                studentlist.Add(student);
+                MessageBox.Show($"{student.Name} Added");
+                textName.Text = "";
+                textSurname.Text = "";
+                selecGroup.Text = null;
+                dataGridView1.DataSource = "";
+                dataGridView1.DataSource = studentlist;
+                
+            }
+
+
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            dataGridView1.DataSource = new List<string>();
-        }
+
 
         private void selecGroup_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            buttonUpdate.Visible = true;
+            or.Visible = true;
+            indexrow = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[indexrow];
+            textName.Text = row.Cells[1].Value.ToString();
+            textSurname.Text = row.Cells[2].Value.ToString();
+            selecGroup.Text = row.Cells[3].Value.ToString();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+
+            DataGridViewRow newdataRow = dataGridView1.Rows[indexrow];
+            newdataRow.Cells[1].Value = textName.Text;
+            newdataRow.Cells[2].Value = textSurname.Text;
+            newdataRow.Cells[3].Value = selecGroup.Text;
+        }
+
+
+
+
+
+
+
     }
+
+
     public class Student
     {
+        private static int count = 0;
+        private int _id;
+        public int Id
+        {
+            get
+            { 
+                return _id;
+            }
+        }
+
         public string Name { get; set; }
         public string Surname { get; set; }
-        public Groups GroupsId { get; set; }
+        public string GroupsId { get; set; }// groups tipinde verdikde if sertinin icindeki groupname eror verrir
         public Student(string n, string s)
         {
+            count++;   
+            _id=count;
             Name = n;
             Surname = s;
         }
+
+
     }
-    public class Groups
+    public class Groupss
     {
+
         private static int count = 1;
         private int _id;
         public int Id
@@ -86,13 +175,16 @@ namespace WindowsForm
             }
         }
         public string GName { get; set; }
-        public List<Student> StudentsList { get; set; } = new List<Student>();
-        public Groups(string g)
+        //public List<Student> StudentList { get; set; } = new List<Student>();
+        public Groupss(string g)
         {
             GName = g;
             _id = count;
             count++;
 
         }
+
+
     }
+
 }
